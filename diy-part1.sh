2a39d53d6da696f305646921b7b0915b41e3d924
GaoMing_18000211 <42202783+SusuDOS@users.noreply.github.com>
2023-07-09 18:59:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,10 +14,6 @@
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-echo 'src-git packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
-echo 'src-git luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
-echo 'src-git routing https://github.com/coolsnowwolf/routing' >>feeds.conf.default
-echo 'src-git telephony https://git.openwrt.org/feed/telephony.git' >>feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
 echo 'src-git oui https://github.com/zhaojh329/oui.git' >>feeds.conf.default
 echo 'src-git video https://github.com/openwrt/video.git' >>feeds.conf.default
